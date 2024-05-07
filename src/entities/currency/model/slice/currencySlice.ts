fb6254d189741248a8b8211fec5dd8ac240d9981
SenezAr <danila.lusuy@gmail.com>
2024-05-07 17:41:11 +0300

    feat: add currency slice and reducers

--- src/entities/currency/model/slice/currencySlice.ts
+++ src/entities/currency/model/slice/currencySlice.ts
@@ -1,6 +1,30 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { CurrencySchema } from "../types/currency";
+import { type Currency } from "../types/currency";
+const initialState: CurrencySchema = {
+  getCurrency: null,
+  giveCurrency: null,
+  getCashCurrency: null,
+  giveCashCurrency: null,
+};
 
-// export const currencySlice = createSlice({
-//     name:"currency",
-//     reducers:
-// })
+export const currencySlice = createSlice({
+  name: "currency",
+  initialState,
+  reducers: {
+    setGetCurrency: (state, action: PayloadAction<Currency>) => {
+      state.getCurrency = action.payload;
+    },
+    setGiveCurrency: (state, action: PayloadAction<Currency>) => {
+      state.giveCurrency = action.payload;
+    },
+    setGetCashCurrency: (state, action: PayloadAction<Currency>) => {
+      state.getCashCurrency = action.payload;
+    },
+    setGiveCashCurrency: (state, action: PayloadAction<Currency>) => {
+      state.giveCashCurrency = action.payload;
+    },
+  },
+});
+export const { actions: currencyActions } = currencySlice;
+export const { reducer: currencyReducer } = currencySlice;
