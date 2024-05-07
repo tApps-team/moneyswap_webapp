4ea9ec1457a986e729911e010f7194ceb4683b8b
SenezAr <danila.lusuy@gmail.com>
2024-05-07 17:40:45 +0300

    feat: add public api

--- src/entities/currency/index.ts
+++ src/entities/currency/index.ts
@@ -1,1 +1,12 @@
 export { useAvailableValutesQuery, currencyApi } from "./api/currencyApi";
+export {
+  getCashCurrency,
+  getCurrency,
+  giveCashCurrency,
+  giveCurrency,
+} from "./model/selectors/currencySelectors";
+export {
+  currencySlice,
+  currencyActions,
+  currencyReducer,
+} from "./model/slice/currencySlice";
