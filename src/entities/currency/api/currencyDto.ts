eddb93af5353f358aae70dbce0845c4de5ab582b
SenezAr <danila.lusuy@gmail.com>
2024-05-08 15:48:10 +0300

    fix: change types path

--- src/entities/currency/api/currencyDto.ts
+++ src/entities/currency/api/currencyDto.ts
@@ -1,4 +1,4 @@
-import { CurrencyCategory } from "../model/types/types";
+import { CurrencyCategory } from "../model/types/currency";
 
 export type AvailableValutesDtoResponse = CurrencyCategory;
 export type AvailableValutesDtoRequest = {
