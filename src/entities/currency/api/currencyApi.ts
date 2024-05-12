32815a634ee5d9fb15ffb21fa65dc2a170349101
Dania <danila.lusuy@gmail.com>
2024-05-13 00:46:40 +0300

    feat(refactoring later): add endpoints and transorm response

--- src/entities/currency/api/currencyApi.ts
+++ src/entities/currency/api/currencyApi.ts
@@ -7,28 +7,31 @@ import {
 export const currencyApi = baseApi.injectEndpoints({
   endpoints: (build) => ({
     availableValutes: build.query<
-      // {
-      //   currencies: AvailableValutesDtoResponse;
-      //   filteredCategories: { ru: string[]; en: string[] };
-      // },
-      AvailableValutesDtoResponse,
+      {
+        currencies: AvailableValutesDtoResponse;
+        filteredCategories: { ru: string[]; en: string[] };
+      },
+      // AvailableValutesDtoResponse,
       AvailableValutesDtoRequest
     >({
-      query: ({ base = "all", city }) => ({
+      query: ({ base, city }) => ({
         url: city
           ? `/api/available_valutes?city=${city}&base=${base}`
           : `/api/available_valutes?base=${base}`,
         method: "GET",
       }),
-      // transformResponse: (response: AvailableValutesDtoResponse, meta, arg) => {
-      //   return {
-      //     currencies: response,
-      //     filteredCategories: {
-      //       ru: Object.keys(response.ru),
-      //       en: Object.keys(response.en),
-      //     },
-      //   };
-      // },
+      transformResponse: (response: AvailableValutesDtoResponse, meta, arg) => {
+        return {
+          currencies: {
+            ru: Object.values(response.ru).flat(),
+            en: Object.values(response.en).flat(),
+          },
+          filteredCategories: {
+            ru: Object.keys(response.ru),
+            en: Object.keys(response.en),
+          },
+        };
+      },
     }),
   }),
 });
