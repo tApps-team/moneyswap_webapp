0391f0203e59bec90bb6a001c2c604d4636be7f1
Dania <danila.lusuy@gmail.com>
2024-05-13 00:46:04 +0300

    fix: remove query provider

--- src/app/providers/withStore.tsx
+++ src/app/providers/withStore.tsx
@@ -2,20 +2,16 @@ import { Provider } from "react-redux";
 import { setupStore } from "./storeProvider/appStore";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from "redux-persist";
-import { QueryClientProvider } from "react-query";
-import { queryClient } from "@/legacy/api/queryClient";
 
 const store = setupStore();
-// const persistor = persistStore(store);
+const persistor = persistStore(store);
 
 export const withStore = (Component: React.FC) => {
   return () => (
-    <QueryClientProvider client={queryClient}>
-      <Provider store={store}>
-        {/* <PersistGate loading={null} persistor={persistor}> */}
-        <Component />
-        {/* </PersistGate> */}
-      </Provider>
-    </QueryClientProvider>
+    <Provider store={store}>
+      {/* <PersistGate loading={null} persistor={persistor}> */}
+      <Component />
+      {/* </PersistGate> */}
+    </Provider>
   );
 };
