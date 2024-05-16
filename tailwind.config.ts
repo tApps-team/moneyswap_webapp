0d53c50365a96dafbf39d5d29f8155ec6db5fd95
Dania <danila.lusuy@gmail.com>
2024-05-16 10:33:36 +0300

    feat: add colors variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,10 +2,10 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   prefix: "",
   theme: {
@@ -18,6 +18,10 @@ module.exports = {
     },
     extend: {
       colors: {
+        mainColor: "#F6FF5F",
+        whiteColor: "#FFF",
+        darkGray: "#2D2D2D",
+        lightGray: "#BBBBBB",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
@@ -74,4 +78,4 @@ module.exports = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
