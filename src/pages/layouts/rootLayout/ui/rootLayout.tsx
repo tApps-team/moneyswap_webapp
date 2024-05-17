3aee0b47c3cb71383aee0ff76acf767608ba6eff
Dania <danila.lusuy@gmail.com>
2024-05-17 11:25:48 +0300

    feat: add toaster

--- src/pages/layouts/rootLayout/ui/rootLayout.tsx
+++ src/pages/layouts/rootLayout/ui/rootLayout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import styles from "./rootLayout.module.scss";
+import { Toaster } from "@/shared/ui";
 
 export const RootLayout = () => {
   return (
     <div className={styles.root__container}>
+      <Toaster />
       <main className={styles.root__content}>
         <Outlet />
       </main>
