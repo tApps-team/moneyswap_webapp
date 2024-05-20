7228b90c68f5bfc12c2f74556cf0e027891625cf
Dania <danila.lusuy@gmail.com>
2024-05-20 10:34:53 +0300

    feat: add active props and active styles

--- src/entities/currency/ui/currencyCard.tsx
+++ src/entities/currency/ui/currencyCard.tsx
@@ -1,15 +1,23 @@
 import { Card, CardContent } from "@/shared/ui";
 import { Currency } from "../model/types/currency";
+import { cx } from "class-variance-authority";
 
 type CurrencyCardProps = {
   currency: Currency;
   onClick?: () => void;
+  active?: boolean;
 };
 export const CurrencyCard = (props: CurrencyCardProps) => {
-  const { currency, onClick } = props;
+  const { currency, onClick, active } = props;
   return (
-    <Card className="h-[70px] rounded-full border-2  " onClick={onClick}>
-      <CardContent className="grid h-full   grid-flow-col px-4 py-2 justify-start  gap-3 items-center">
+    <Card
+      className={cx(
+        "h-[70px] rounded-full border-2 active:bg-mainColor bg-darkGray text-white",
+        active && "bg-mainColor border-mainColor text-black"
+      )}
+      onClick={onClick}
+    >
+      <CardContent className="grid h-full   grid-flow-col px-4 py-2 justify-start  gap-3 gap-y-0 items-center">
         <img
           className="row-span-2"
           src={currency.icon_url}
@@ -17,7 +25,7 @@ export const CurrencyCard = (props: CurrencyCardProps) => {
           width={34}
           height={34}
         />
-        <div className="truncate">{currency.name}</div>
+        <div className="truncate font-bold">{currency.name}</div>
         <div className="truncate">{currency.code_name}</div>
       </CardContent>
     </Card>
