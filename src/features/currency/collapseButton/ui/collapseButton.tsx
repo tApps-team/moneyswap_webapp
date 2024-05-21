89e8e3b2e659a328248e36bd72105bdd8dcd8c05
Dania <danila.lusuy@gmail.com>
2024-05-21 16:36:06 +0300

    feat: add collapse logic

--- src/features/currency/collapseButton/ui/collapseButton.tsx
+++ src/features/currency/collapseButton/ui/collapseButton.tsx
@@ -4,6 +4,7 @@ import { cx } from "class-variance-authority";
 type CollapseButtonProps = {
   currentGiveCurrency: boolean;
   currentGetCurrencies: boolean;
+  isCollapse: boolean;
   currenctExchangersIsSuccessState: boolean;
   onClick: () => void;
 };
@@ -11,24 +12,26 @@ export const CollapseButton = (props: CollapseButtonProps) => {
   const {
     currentGiveCurrency,
     currentGetCurrencies,
+    isCollapse,
     currenctExchangersIsSuccessState,
     onClick,
   } = props;
+
   return (
     <Button
       onClick={onClick}
       disabled={!currenctExchangersIsSuccessState}
       className={cx(
-        "w-1/3  flex justify-center bottom-0 disabled:opacity-100  items-center absolute left-[50%] right-[50%] -translate-x-1/2 translate-y-3  h-6 rounded-full",
-        !currentGiveCurrency || !currentGetCurrencies
-          ? "bg-lightGray"
-          : "bg-mainColor border-2 border-darkGray "
+        "w-1/3 transition duration-1000 ease-in-out flex justify-center bottom-0 disabled:opacity-100  items-center absolute left-[50%] right-[50%] -translate-x-1/2 translate-y-3  h-7 rounded-full",
+        currenctExchangersIsSuccessState
+          ? "bg-mainColor border-2 border-darkGray w-1/4 "
+          : "bg-lightGray"
       )}
     >
       <LogoArrow
         className={cx(
           "transition duration-300 size-6 ease-in-out",
-          currenctExchangersIsSuccessState ? "rotate-90" : "-rotate-90 "
+          isCollapse ? "-rotate-90" : "rotate-90 "
         )}
       />
     </Button>
