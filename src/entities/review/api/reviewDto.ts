34306b568c9dedb9cb0188eb59fcbfefbc3352b4
Dania <danila.lusuy@gmail.com>
2024-05-23 10:20:40 +0300

    feat: add types

--- src/entities/review/api/reviewDto.ts
+++ src/entities/review/api/reviewDto.ts
@@ -1,10 +1,14 @@
-import { CurrencyType, ReviewResponse } from "../model/types/reviewType";
+import {
+  ExchangerMarker,
+  Grade,
+  ReviewResponse,
+} from "../model/types/reviewType";
 
 export type ReviewsByExchangeDtoResponse = ReviewResponse;
 export type ReviewsByExchangeDtoRequest = {
   exchange_id: number;
-  exchange_marker: CurrencyType;
+  exchange_marker: ExchangerMarker;
   page: number;
-  element_on_page: number;
-  grade_filter: number;
+  element_on_page?: number;
+  grade_filter?: Grade;
 };
