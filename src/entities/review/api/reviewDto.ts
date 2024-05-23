194c9b9c2794b35cb23ef0e34953618b6659d4c5
SenezAr <danila.lusuy@gmail.com>
2024-05-23 16:00:32 +0300

    feat: add types

--- src/entities/review/api/reviewDto.ts
+++ src/entities/review/api/reviewDto.ts
@@ -12,3 +12,22 @@ export type ReviewsByExchangeDtoRequest = {
   element_on_page?: number;
   grade_filter?: Grade;
 };
+
+export type AddReviewByExchangeDtoResponse = string;
+export type AddReviewByExchangeDtoRequset = {
+  exchange_id: number;
+  exchange_marker: ExchangerMarker;
+  tg_id: number;
+  text: string;
+  grade: Grade;
+  transaction_id?: string | null;
+};
+
+export type CheckUserReviewPermissionDtoResponse = {
+  status: "success";
+};
+export type CheckUserReviewPermissionDtoRequest = {
+  exchange_id: number;
+  exchange_marker: ExchangerMarker;
+  tg_id: number;
+};
