710266648ca29d3034e8cc9520b1016064c79370
SenezAr <danila.lusuy@gmail.com>
2024-05-28 16:10:19 +0300

    feat: add grade query

--- src/features/review/reviewList/ui/reviewList.tsx
+++ src/features/review/reviewList/ui/reviewList.tsx
@@ -8,8 +8,16 @@ import {
 } from "@/entities/review";
 import { selectCacheByKey } from "@/entities/review/api/reviewApi";
 import { baseApi } from "@/shared/api";
-import { ScrollArea, ScrollBar } from "@/shared/ui";
+import {
+  ScrollArea,
+  ScrollBar,
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/shared/ui";
 import { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
 import { useInView } from "react-intersection-observer";
 import { useSelector } from "react-redux";
 type ReviewListProps = {
@@ -18,10 +26,13 @@ type ReviewListProps = {
 };
 export const ReviewList = (props: ReviewListProps) => {
   const { exchanger, isOpen } = props;
+  const { t } = useTranslation();
+  const [grade, setGrade] = useState<Grade>(Grade.all);
   const cachePage = useSelector(selectCacheByKey(exchanger?.exchange_id));
-  const [page, setPage] = useState<number>(cachePage || 1);
+  const [page, setPage] = useState<number>(cachePage?.page || 1);
+  console.log(cachePage?.content);
   const { ref, inView, entry } = useInView({
-    threshold: 1,
+    threshold: 0,
     triggerOnce: true,
   });
   useEffect(() => {
@@ -36,21 +47,72 @@ export const ReviewList = (props: ReviewListProps) => {
       exchange_marker: exchanger.exchange_marker,
       page: page,
       element_on_page: 3,
-      grade_filter: Grade.neutral,
+      grade_filter: grade === Grade.all ? undefined : grade,
     },
     {
       skip: !isOpen,
     }
   );
 
+  const tabItems: {
+    tabValue: Grade;
+    tabName: string;
+    tabReviewValue: number;
+  }[] = [
+    {
+      tabValue: Grade.all,
+      tabName: t("ВСЕ"),
+      tabReviewValue:
+        exchanger?.review_count.negative +
+        exchanger?.review_count.neutral +
+        exchanger?.review_count.positive,
+    },
+    {
+      tabValue: Grade.positive,
+      tabName: t("ПОЛОЖИТЕЛЬНЫЕ"),
+      tabReviewValue: exchanger?.review_count.positive,
+    },
+    {
+      tabValue: Grade.neutral,
+
+      tabName: t("НЕЙТРАЛЬНЫЕ"),
+      tabReviewValue: exchanger?.review_count.neutral,
+    },
+    {
+      tabValue: Grade.negative,
+      tabName: t("ОТРИЦАТЕЛЬНЫЕ"),
+      tabReviewValue: exchanger?.review_count.negative,
+    },
+  ];
   return (
-    <ScrollArea className="w-full p-2">
-      <div className="grid gap-2">
-        {reviews?.content.map((review) => (
-          <ReviewCard ref={ref} key={review.id} review={review} />
+    <div>
+      <Tabs onValueChange={(e) => setGrade(e)} className="">
+        <TabsList className="bg-transparent grid grid-rows-2 grid-cols-2 gap-2 h-full ">
+          {tabItems.map((tab) => (
+            <TabsTrigger
+              className="rounded-3xl bg-darkGray   uppercase data-[state=active]:text-black data-[state=active]:border-mainColor text-white border-2 h-11 data-[state=active]:bg-mainColor"
+              value={tab.tabValue}
+            >
+              <div className="flex truncate items-center gap-1">
+                <p className="truncate text-xs">{tab.tabName}</p>
+                <p className=" text-xs">({tab.tabReviewValue})</p>
+              </div>
+            </TabsTrigger>
+          ))}
+        </TabsList>
+        {tabItems.map((tab) => (
+          <TabsContent value={tab.tabValue}>
+            <ScrollArea className=" h-[70svh] w-full p-2">
+              <div className="grid gap-2">
+                {reviews?.content.map((review) => (
+                  <ReviewCard ref={ref} key={review.id} review={review} />
+                ))}
+              </div>
+              <ScrollBar className="" />
+            </ScrollArea>
+          </TabsContent>
         ))}
-      </div>
-      <ScrollBar className="" />
-    </ScrollArea>
+      </Tabs>
+    </div>
   );
 };
