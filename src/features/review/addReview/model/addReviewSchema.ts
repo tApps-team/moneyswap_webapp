39061ba1fef9fa17d31eba8edc2c81514d3cf620
Dania <danila.lusuy@gmail.com>
2024-05-31 01:36:15 +0300

    feat: add validate transaction id

--- src/features/review/addReview/model/addReviewSchema.ts
+++ src/features/review/addReview/model/addReviewSchema.ts
@@ -1,10 +1,15 @@
 import { Grade } from "@/entities/review";
 import { z } from "zod";
-export const addReviewSchema = z.object({
-  review: z.string().min(1).max(2000),
-  grade: z.enum(["1", "0", "-1"], {
-    required_error: "Поле обзятельно",
-  }),
-  transaction_id: z.string(),
-});
+export const addReviewSchema = z
+  .object({
+    review: z.string().min(1).max(2000),
+    grade: z.enum(["1", "0", "-1"], {
+      required_error: "Поле обзятельно",
+    }),
+    transaction_id: z.string().optional(),
+  })
+  .refine((data) => data.grade !== "-1" || data.transaction_id, {
+    message: "transaction_id is required for negative reviews",
+    path: ["transaction_id"],
+  });
 export type AddReviewSchemaType = z.infer<typeof addReviewSchema>;
