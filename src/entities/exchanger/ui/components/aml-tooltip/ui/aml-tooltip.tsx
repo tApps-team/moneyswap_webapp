ec10a05b22c0695f26ec7936369e6c245c83aee1
AbddSsh <abduraimovshakh@gmail.com>
2025-04-07 15:14:07 +0500

    fix: AML in card

--- src/entities/exchanger/ui/components/aml-tooltip/ui/aml-tooltip.tsx
+++ src/entities/exchanger/ui/components/aml-tooltip/ui/aml-tooltip.tsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
-import { AMLMobileIcon } from "@/shared/assets";
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/shared/ui";
 import { useTranslation } from "react-i18next";
 
-export const AMLTooltip = () => {
+interface AMLTooltipProps {
+  isHighRisk: boolean;
+}
+
+export const AMLTooltip = ({ isHighRisk }: AMLTooltipProps) => {
     const [open, setOpen] = useState(false);
     const { t } = useTranslation();
     return (
@@ -11,7 +14,7 @@ export const AMLTooltip = () => {
       <Tooltip open={open}>
         <TooltipTrigger
           type="button"
-          className="!z-[15]"
+          className="!z-[15] rounded-[3px] bg-new-light-grey py-0.5 px-1 w-fit"
           onClick={(e) => {
             e.stopPropagation();
             setOpen(!open);
@@ -20,13 +23,17 @@ export const AMLTooltip = () => {
           onMouseEnter={() => setOpen(true)}
           onMouseLeave={() => setOpen(false)}
         >
-          <AMLMobileIcon />
+          <p className="mobile:text-xs text-[10px] text-mainColor">
+            AML risk: <span className={`${isHighRisk ? 'text-red-500' : 'text-green-500'} font-medium`}>
+              {isHighRisk ? 'HIGH' : 'LOW'}
+            </span>
+          </p>
         </TooltipTrigger>
         <TooltipContent
           side="bottom"
           className="shadow-lg !z-[20] bg-new-light-grey rounded-[8px] lg:px-4 lg:py-3 px-2 py-2 border-none w-[70vw] max-w-[200px]"
         >
-          <p className="leading-3 mobile-xl:text-xs text-[10px] text-white text-wrap whitespace-normal overflow-wrap-anywhere">{t("AML")}</p>
+          <p className="leading-3 mobile-xl:text-xs text-[10px] text-white text-wrap whitespace-normal overflow-wrap-anywhere">{t(isHighRisk ? "AML_high" : "AML_low")}</p>
         </TooltipContent>
       </Tooltip>
       </TooltipProvider>
